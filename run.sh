f352d05e7a8e44d8a7d9b93fc417ea6f516465bd
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-11-21 18:37:51 +0000

    run in docker

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ./run.sh tutorial_basic_functionality
+/isaac-sim/runheadless.native.sh  --no-window --/omni/replicator/replicatorYaml/yamlPath=/workspaces/synthetic-data-examples/$1.yaml

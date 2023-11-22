22e75fc45895566ac32fe1173bf6a1531b328d46
Wojciech Turowicz <wojciech.turowicz@surveily.com>
2023-11-22 08:06:41 +0000

    it works in container

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # ./run.sh tutorial_basic_functionality
-/isaac-sim/runheadless.native.sh  --no-window --/omni/replicator/replicatorYaml/yamlPath=/workspaces/synthetic-data-examples/$1.yaml
+/isaac-sim/runheadless.native.sh  --no-window \
+                                  --/omni/replicator/replicatorYaml/yamlPath=/workspaces/synthetic-data-examples/omni.replicator_yaml/$1.yaml
